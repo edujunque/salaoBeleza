3ddbefcc1b4e96d999bc1444d2cbd50be28ac798
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-09-21 17:31:06 -0300

    atualizando master

--- SalaoBeleza/Scripts/dataTableCall.js
+++ SalaoBeleza/Scripts/dataTableCall.js
@@ -1,25 +1,15 @@
 ﻿$(document).ready(function () {
-    //$('.table').DataTable({
-    //    "columnDefs": [{
-    //        "targets": 'no-sort',
-    //        "orderable": false,
-    //    }],
-    //    "paging": true,
-    //    "lengthChange": true,
-    //    "searching": true,
-    //    "ordering": true,
-    //    "info": true,
-    //    "autoWidth": false,
-    //    "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
-    //});
-    $(function () {
-        $('.table').DataTable({
-            'paging': true,
-            'lengthChange': false,
-            'searching': true,
-            'ordering': true,
-            'info': true,
-            'autoWidth': false
-        })
-    })
+    $('.table').DataTable({
+        "columnDefs": [{
+            "targets": 'no-sort',
+            "orderable": false,
+        }],
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+    });
 });
